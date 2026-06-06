47d16e6940e592e9473933117140e54db7050156
leonidas400 <dilvano639@gmail.com>
2026-06-06 10:30:29 -0300

    concertando problemas de envio de e-mail

--- servidor/mailer.py
+++ servidor/mailer.py
@@ -17,6 +17,14 @@ conf = ConnectionConfig(
 )
 
 async def enviar_notificacao_email(filename: str, device_id: str):
+
+    print(f"\n[DEBUG EMAIL] ---> Iniciando envio para o arquivo: {filename} <--- \n")
+    
+    if not SMTP_USER or not EMAIL_DESTINO:
+        print("[DEBUG EMAIL] ---> ERRO: Credenciais SMTP estão vazias no .env! <---")
+        log.warning("Credenciais SMTP não configuradas. Pulando envio de e-mail.")
+        return
+    
     if not SMTP_USER or not EMAIL_DESTINO:
         log.warning("Credenciais SMTP não configuradas. Pulando envio de e-mail.")
         return
@@ -37,6 +45,8 @@ async def enviar_notificacao_email(filename: str, device_id: str):
     fm = FastMail(conf)
     try:
         await fm.send_message(message)
+        print(f"\n[DEBUG EMAIL] ---> SUCESSO: E-mail enviado! <---\n")
         log.info(f"E-mail de notificação enviado com sucesso para {filename}")
     except Exception as e:
+        print(f"\n[DEBUG EMAIL] ---> FALHA CRÍTICA AO ENVIAR: {e} <---\n")
         log.error(f"Falha ao enviar e-mail: {e}")
